d7953f3b78a6965b415cfdae6b1d75e1f4df66bf
hasibzunair <hasibzunair@gmail.com>
2022-06-13 03:33:03 -0400

    assert statements

--- store_hparams/main.py
+++ store_hparams/main.py
@@ -2,7 +2,10 @@ from config import *
 
 # See https://twitter.com/kagglingdieter/status/1531155343505489920 for details
 # Also see https://github.com/DrSleep/light-weight-refinenet/blob/master/src/train.py
+# And https://github.com/google/ml_collections
 
+assert type(DATA_DIR) == str, f"Should be a path to dataset, got: {type(DATA_DIR)}"
+assert type(LR) == float, f"Should be a float, got: {type(LR)}"
 
 print(f"Path to data is at {DATA_DIR}.\n")
 print(f"Pretraining is {PRETRAINED}.\n")
