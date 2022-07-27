5a3adb1d8968c3cf7a00b50cc769276d1154c715
hasibzunair <hasibzunair@gmail.com>
2022-07-27 14:16:48 -0600

    update resize.py: resize and keep aspect ratio same

--- resusable/resize.py
+++ resusable/resize.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
-from PIL import Image
-from PIL import ImageOps
+from PIL import Image, ImageOps
 
 # Code commenting reference:
 # https://github.com/hasibzunair/res-unet-fastmri/blob/master/fastMRI/data/transforms.py
@@ -21,10 +20,13 @@ def resize_image(image_path, new_width=192, new_height=256):
     assert type(image_path) == str, f"Should be a path, got: {image_path} which is {type(image_path)}"
     
     img = Image.open(image_path)
-    img = ImageOps.fit(image_path, (new_width, new_height), Image.ANTIALIAS)
+    img = ImageOps.fit(image_path, (new_width, new_height), Image.BICUBIC)
     img = img.convert("RGB")
     img = np.array(img)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     # If need saving
-    #cv2.imwrite("path.jpg", img)
+    # using cv2
+    #cv2.imwrite("path.png", img)
+    # or using PIL
+    # img.save("path.png")
     return img
