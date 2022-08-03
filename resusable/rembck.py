6f02abf12f5fbd90e3cc3c71e23e84c0955b21d5
hasibzunair <hasibzunair@gmail.com>
2022-08-03 13:58:41 -0600

    save background removed image

--- resusable/rembck.py
+++ resusable/rembck.py
@@ -9,4 +9,11 @@ def read_image(path):
 
 def remove_background(image):
     image = remove(image)
+
+    # To save, do:
+    # image.save("name.png")
+    
+    # For black background: 
+    # cv2.imwrite("name.png", cv2.cvtColor(np.array(a.convert("RGB")), cv2.COLOR_RGB2BGR))
     return image
+
